7629b40895046285e660b52a1fb64a7a75c3d715
Rossanne <rossanneh@gmail.com>
2023-01-21 11:23:54 +0000

    Tidy playGame function

--- script.js
+++ script.js
@@ -9,15 +9,6 @@ function onlyLetters(str) { //returns true if string only contains letters, fals
 }
 
 function playGame() { //alert boxes for getting player name and starting game
-	
-	// if ((playerName.length === 3) && (onlyLetters(playerName))) {
-	// 	alert("Thank you " + playerName.toUpperCase() + "! Are you ready to smash some worms?");
-	// 	countdown(1);
-    //     startAnimation();
-    //     gameIsPlaying = true;
-	// } else {
-	// 	playGame();
-	// }
 
     if (gameIsPlaying === true) {
         alert("Did you mean to pause the game? Game is paused. Press 'OK' when you want to continue, or 'restart' if you want to start a new game");
